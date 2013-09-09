fdc75f1b6bfac08b9511c2e4abd38a444cc5c5f5
Tobias Megies <megies@geophysik.uni-muenchen.de>
2013-09-09 16:16:52 +0200

    refactoring: move plotting in a Tkinter GUI and run Seedlink Client in separate thread.

--- fog_compass.py
+++ fog_compass.py
@@ -1,6 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import matplotlib
+# Set the backend for matplotlib.
+matplotlib.use("TkAgg")
+from matplotlib.figure import Figure
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+import Tkinter
+
 from obspy.core.utcdatetime import UTCDateTime
 from obspy import *
 from obspy.seedlink.seedlinkexception import SeedLinkException
@@ -12,6 +19,8 @@ import traceback
 import logging
 import math as m
 import matplotlib.pyplot as plt
+from copy import deepcopy
+import threading
 
 
 # default logger
@@ -22,12 +31,15 @@ class MySLClient(SLClient):
     """
     A custom SeedLink client.
     """
-    def __init__(self,stream, *args, **kwargs):
+    def __init__(self, lock, shared, *args, **kwargs):
         """
         Creates a new instance of SLClient accepting a realtime trace handler.
         """
-        self.stream = stream
-        self.history = []
+        self.lock = lock
+        self.history = shared['history']
+        self.pitch = shared['pitch']
+        self.roll = shared['roll']
+        self.stream = Stream()
         super(self.__class__, self).__init__(*args, **kwargs)
         self.starttimes_ok = False
 
@@ -141,16 +153,85 @@ class MySLClient(SLClient):
         # azimuth vs north
         azimuth = -m.atan2(rot_vec_cor[1],rot_vec_cor[0]) *  180/m.pi
         
-        
-        self.history.append(azimuth)
-        short_history = self.history[-10:]
+        # calculate pitch and roll
+        pitch = m.degrees(theta)
+        roll = m.degrees(phi)
+
+        with self.lock:
+            self.history.append(azimuth)
+            self.pitch.append(pitch)
+            self.roll.append(roll)
+
+        self.stream.trim(starttime=t2)
+
+        return False
+
+
+class AzimuthPlotter(Tkinter.Tk):
+    """
+    """
+    def __init__(self, lock, shared, *args, **kwargs):
+        Tkinter.Tk.__init__(self, *args, **kwargs)
+
+        self.lock = lock
+        self.history = shared['history']
+        self.pitch = shared['pitch']
+        self.roll = shared['roll']
+        self.update_interval = 500  # in milliseconds
+
+        self.focus_set()
+        self._bind_keys()
+        ### size and position
+        self.geometry("400"+'x'+"400"+'+'+"100"+'+'+"100")
+        w, h, pad = self.winfo_screenwidth(), self.winfo_screenheight(), 3
+        self._geometry = ("%ix%i+0+0" % (w - pad, h - pad))
+
+        # main figure
+        self.fig = Figure()
+        self.ax = self.fig.add_subplot(1, 1, 1, polar=True)
+
+        canvas = FigureCanvasTkAgg(self.fig, master=self)
+        canvas.show()
+        canvas.get_tk_widget().pack(fill=Tkinter.BOTH, expand=1)
+        self.canvas = canvas
+
+        self.plot()
+
+    def _quit(self, event):
+        event.widget.quit()
+
+    def _bind_keys(self):
+        self.bind('<Escape>', self._quit)
+        self.bind('q', self._quit)
+        self.bind('f', self._toggle_fullscreen)
+
+    def _toggle_fullscreen(self, event):
+        g = self.geometry()
+        self.geometry(self._geometry)
+        self._geometry = g
+
+    def plot(self):
+        with self.lock:
+            short_history = deepcopy(self.history[-10:])
+            pitch = deepcopy(self.pitch[-10:])
+            roll = deepcopy(self.roll[-10:])
+
+        try:
+            azimuth = short_history[-1]
+            pitch = pitch[-1]
+            roll = roll[-1]
+        # no data to plot yet
+        except IndexError:
+            self.after(self.update_interval, self.plot)
+            return
+
         std_history = np.std(short_history)
 
         #print "Mean History %f\n"%(mean_history)
         print "Standart Deviation History %f\n"%(std_history)
 
         print "Azimuth[deg]: %f\n"%(azimuth)
-        print  "Pitch: %f, Roll: %f\n"%(theta*180./m.pi,phi*180./m.pi)
+        print  "Pitch: %f, Roll: %f\n"%(pitch, roll)
 
         # Plot
         azimuth_rad = np.deg2rad(azimuth)
@@ -159,48 +240,41 @@ class MySLClient(SLClient):
         radii = 1
         width = 2 * std_rad
         
-        
-        ax = plt.subplot(1, 1, 1, polar=True)
-        ax.annotate("Azimuth: %f\n"%(azimuth),
+        ax = self.ax
+        ax.clear()
+        ax.set_theta_direction(-1)
+        ax.set_theta_zero_location("N")
+        ax.annotate("Azimuth: %.1f\n"%(azimuth),
                     xy=(azimuth_rad,radii),
                     xytext=(0.02, 0.11),
                     textcoords='figure fraction')
-        ax.annotate("Std Azimuth: %f\n"%(std_history),
+        ax.annotate("Std Azimuth: %.1f\n"%(std_history),
                     xy=(azimuth_rad,radii),
                     xytext=(0.02, 0.08),
                     textcoords='figure fraction')
-        ax.annotate("pitch: %f\n"%(theta),
+        ax.annotate("pitch: %.1f\n"%(pitch),
                     xy=(azimuth_rad,radii),
                     xytext=(0.02, 0.03),
                     textcoords='figure fraction')
-        ax.annotate("roll: %f\n"%(phi),
+        ax.annotate("roll: %f\n"%(roll),
                     xy=(azimuth_rad,radii),
                     xytext=(0.02, 0.0),
                     textcoords='figure fraction')
-        ax.set_theta_direction(-1)
-        ax.set_theta_zero_location("N")
         bars = ax.bar(azimuth_rad - 0.5 * width, radii, width=width, alpha=0.2)
-        plt.plot([azimuth_rad] * 2, [0, 1], color="r", lw=2)
+        ax.plot([azimuth_rad] * 2, [0, 1], color="r", lw=2)
+        self.fig.canvas.draw()
 
-
-        plt.show()
-
-                
-        
-        self.stream.trim(starttime=t2)
-                
-        return False
-
+        self.after(self.update_interval, self.plot)
 
 
 def main():
-    # initialize realtime trace
-    stream = Stream()
+    # initialize objects shared between threads
+    lock = threading.Lock()
+    shared = {'history': [], 'pitch': [], 'roll': []}
 
     # create SeedLink client
-    slClient = None
     try:
-        slClient = MySLClient(stream)
+        slClient = MySLClient(lock, shared)
         #
         #slClient.slconn.setSLAddress("erde:18000")
         #slClient.multiselect = ("BW_FOG1:EJ1 EJ2 EJ3 EN1 EN2 EN3")
@@ -218,7 +292,10 @@ def main():
         print slClient.end_time
         slClient.verbose = 3
         slClient.initialize()
-        slClient.run()
+        # start slClient in a thread
+        thread = threading.Thread(target=slClient.run)
+        thread.setDaemon(True)
+        thread.start()
     except SeedLinkException, sle:
         logger.critical(sle)
         traceback.print_exc()
@@ -228,6 +305,9 @@ def main():
         traceback.print_exc()
         raise e
 
+    # start GUI window
+    gui = AzimuthPlotter(lock, shared)
+    gui.mainloop()
 
 if __name__ == '__main__':
     main()
